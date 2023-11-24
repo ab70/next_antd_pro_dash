ba83ce7b2cc12becc0269d238510c6c1074b4210
ab70 <nurulabrar2369@gmail.com>
2023-11-24 11:03:23 +0600

    finalpush to data

--- app/page.jsx
+++ app/page.jsx
@@ -19,6 +19,9 @@ import { Divider, Space, Tabs, message, theme } from 'antd';
 import { useEffect, useState } from 'react';
 import styles from "./style.module.css"
 import Image from 'next/image'
+import dynamic from 'next/dynamic';
+const LoginPage = dynamic(() => import("./Components/auth_Components/login"))
+const SignUpPage = dynamic(() => import("./Components/auth_Components/signup"))
 // import niamOfficiallogo from "../public/niamofficiallogo.png"
 
 const Home = () => {
@@ -41,7 +44,16 @@ const Home = () => {
         cursor: 'pointer',
     };
     const items = [
-
+        {
+            key: 'phone',
+            label: 'Phone',
+            children: <LoginPage />,
+        },
+        {
+            key: 'account',
+            label: 'Account',
+            children: <SignUpPage />,
+        },
     ]
 
     return (
@@ -55,7 +67,6 @@ const Home = () => {
                             <ProConfigProvider hashed={false} >
                                 <div >
                                     <LoginForm
-                                        // backgroundImageUrl="https://niam-next.vercel.app/images/niamsigninbg.svg"
                                         logo={
                                             <Image
                                                 src="/niamofficiallogo.png"
@@ -75,7 +86,7 @@ const Home = () => {
                                                 <Divider plain>
                                                     Other login methods
                                                 </Divider>
-                                                <Space style={{marginLeft: "25%"}}>
+                                                <Space style={{ marginLeft: "25%" }}>
 
                                                     <AlipayCircleOutlined style={iconStyles} />
                                                     <TaobaoCircleOutlined style={iconStyles} />
@@ -89,105 +100,25 @@ const Home = () => {
                                         <Tabs
                                             centered
                                             activeKey={loginType}
+                                            items={items}
                                             onChange={(activeKey) => setLoginType(activeKey)}
-                                        >
-                                            <Tabs.TabPane key={'account'} tab={'账号密码登录'} />
-                                            <Tabs.TabPane key={'phone'} tab={'手机号登录'} />
-                                        </Tabs>
-                                        {loginType === 'account' && (
-                                            <>
-                                                <ProFormText
-                                                    name="username"
-                                                    fieldProps={{
-                                                        size: 'large',
-                                                        prefix: <UserOutlined className={'prefixIcon'} />,
-                                                    }}
-                                                    placeholder={'用户名: admin or user'}
-                                                    rules={[
-                                                        {
-                                                            required: true,
-                                                            message: '请输入用户名!',
-                                                        },
-                                                    ]}
-                                                />
-                                                <ProFormText.Password
-                                                    name="password"
-                                                    fieldProps={{
-                                                        size: 'large',
-                                                        prefix: <LockOutlined className={'prefixIcon'} />,
-                                                    }}
-                                                    placeholder={'密码: ant.design'}
-                                                    rules={[
-                                                        {
-                                                            required: true,
-                                                            message: '请输入密码！',
-                                                        },
-                                                    ]}
-                                                />
-                                            </>
-                                        )}
-                                        {loginType === 'phone' && (
-                                            <>
-                                                <ProFormText
-                                                    fieldProps={{
-                                                        size: 'large',
-                                                        prefix: <MobileOutlined className={'prefixIcon'} />,
-                                                    }}
-                                                    name="mobile"
-                                                    placeholder={'手机号'}
-                                                    rules={[
-                                                        {
-                                                            required: true,
-                                                            message: '请输入手机号！',
-                                                        },
-                                                        {
-                                                            pattern: /^1\d{10}$/,
-                                                            message: '手机号格式错误！',
-                                                        },
-                                                    ]}
-                                                />
-                                                <ProFormCaptcha
-                                                    fieldProps={{
-                                                        size: 'large',
-                                                        prefix: <LockOutlined className={'prefixIcon'} />,
-                                                    }}
-                                                    captchaProps={{
-                                                        size: 'large',
-                                                    }}
-                                                    placeholder={'请输入验证码'}
-                                                    captchaTextRender={(timing, count) => {
-                                                        if (timing) {
-                                                            return `${count} ${'获取验证码'}`;
-                                                        }
-                                                        return '获取验证码';
-                                                    }}
-                                                    name="captcha"
-                                                    rules={[
-                                                        {
-                                                            required: true,
-                                                            message: '请输入验证码！',
-                                                        },
-                                                    ]}
-                                                    onGetCaptcha={async () => {
-                                                        message.success('获取验证码成功！验证码为：1234');
-                                                    }}
-                                                />
-                                            </>
-                                        )}
+                                        />
+                                           
+
                                         <div
                                             style={{
                                                 marginBlockEnd: 24,
                                             }}
                                         >
                                             <ProFormCheckbox noStyle name="autoLogin">
-                                                自动登录
+                                                Remember
                                             </ProFormCheckbox>
                                             <a
                                                 style={{
                                                     float: 'right',
                                                 }}
                                             >
-                                                忘记密码
+                                            Forgot Password
                                             </a>
                                         </div>
                                     </LoginForm>
